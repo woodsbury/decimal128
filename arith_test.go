20c57de11f3f9356d3abb9cbb58ff9f16497f13e
Wade Smith <wade.cameron@gmail.com>
2023-11-15 22:05:12 +1000

    improve basic operations fuzz test
    Improve the basic operations fuzz test to cover more scenarios.

--- arith_test.go
+++ arith_test.go
@@ -2,6 +2,7 @@ package decimal128
 
 import (
 	"bytes"
+	"fmt"
 	"math/big"
 	"testing"
 )
@@ -135,99 +136,184 @@ func TestDecimalSub(t *testing.T) {
 }
 
 func FuzzOperations(f *testing.F) {
-	f.Fuzz(func(t *testing.T, xneg bool, xlo, xhi uint64, xexp uint16, yneg bool, ylo, yhi uint64, yexp uint16) {
+	const minBinaryExponent = -20517
+
+	f.Fuzz(func(t *testing.T, xlo, xhi, ylo, yhi uint64) {
 		t.Parallel()
 
-		if xhi > 0x0002_7fff_ffff_ffff || yhi > 0x0002_7fff_ffff_ffff {
-			t.Skip()
+		x := Decimal{xlo, xhi}
+		y := Decimal{ylo, yhi}
+
+		var bigx *big.Float
+		var bigy *big.Float
+		var bigres *big.Float
+		if !x.isSpecial() && !y.isSpecial() {
+			bigx = x.Float(nil)
+			bigy = y.Float(nil)
+			bigres = new(big.Float)
 		}
 
-		if xexp > maxBiasedExponent || yexp > maxBiasedExponent {
-			t.Skip()
-		}
-
-		x := compose(xneg, uint128{xlo, xhi}, int16(xexp))
-		y := compose(yneg, uint128{ylo, yhi}, int16(yexp))
-
-		bigx := x.Float(nil)
-		bigy := y.Float(nil)
-		bigres := new(big.Float)
-		var bigbuf []byte
+		var buf []byte
+		var cmpbuf []byte
 
 		res := x.Add(y)
-		var buf []byte
 
-		if !res.isSpecial() {
+		if bigres == nil || res.isSpecial() {
+			buf = res.Append(buf[:0], ".3e")
+			idx := bytes.IndexByte(buf, 'e')
+			if idx != -1 {
+				buf[idx-1] = '0'
+			}
+
+			fltres := x.Float64() + y.Float64()
+			cmpbuf = fmt.Appendf(cmpbuf[:0], "%.3e", fltres)
+			idx = bytes.IndexByte(cmpbuf, 'e')
+			if idx != -1 {
+				cmpbuf[idx-1] = '0'
+			}
+
+			if string(buf) != string(cmpbuf) {
+				t.Fail()
+			}
+		} else {
 			buf = res.Append(buf[:0], ".3e")
 			idx := bytes.IndexByte(buf, 'e')
 			buf[idx-1] = '0'
 
 			bigres.Add(bigx, bigy)
-			if bigexp := bigres.MantExp(nil); bigexp < maxUnbiasedExponent && bigexp > minUnbiasedExponent {
-				bigbuf = bigres.Append(bigbuf[:0], 'e', 3)
-				idx = bytes.IndexByte(bigbuf, 'e')
-				bigbuf[idx-1] = '0'
+			if bigexp := bigres.MantExp(nil); bigexp > minBinaryExponent {
+				cmpbuf = bigres.Append(cmpbuf[:0], 'e', 3)
+				idx = bytes.IndexByte(cmpbuf, 'e')
+				cmpbuf[idx-1] = '0'
 
-				if string(buf) != string(bigbuf) {
+				if string(buf) != string(cmpbuf) {
 					t.Fail()
 				}
+			} else if string(buf) != "0.000e+00" {
+				t.Fail()
 			}
 		}
 
 		res = x.Mul(y)
 
-		if !res.isSpecial() {
+		if bigres == nil || res.isSpecial() {
+			buf = res.Append(buf[:0], ".3e")
+			idx := bytes.IndexByte(buf, 'e')
+			if idx != -1 {
+				buf[idx-1] = '0'
+			}
+
+			fltres := x.Float64() * y.Float64()
+			cmpbuf = fmt.Appendf(cmpbuf[:0], "%.3e", fltres)
+			idx = bytes.IndexByte(cmpbuf, 'e')
+			if idx != -1 {
+				cmpbuf[idx-1] = '0'
+			}
+
+			if string(buf) != string(cmpbuf) {
+				t.Fail()
+			}
+		} else {
 			buf = res.Append(buf[:0], ".3e")
 			idx := bytes.IndexByte(buf, 'e')
 			buf[idx-1] = '0'
 
 			bigres.Mul(bigx, bigy)
-			if bigexp := bigres.MantExp(nil); bigexp < maxUnbiasedExponent && bigexp > minUnbiasedExponent {
-				bigbuf = bigres.Append(bigbuf[:0], 'e', 3)
-				idx = bytes.IndexByte(bigbuf, 'e')
-				bigbuf[idx-1] = '0'
+			if bigexp := bigres.MantExp(nil); bigexp > minBinaryExponent {
+				cmpbuf = bigres.Append(cmpbuf[:0], 'e', 3)
+				idx = bytes.IndexByte(cmpbuf, 'e')
+				cmpbuf[idx-1] = '0'
 
-				if string(buf) != string(bigbuf) {
+				if string(buf) != string(cmpbuf) {
 					t.Fail()
 				}
+			} else if string(buf) != "0.000e+00" {
+				t.Fail()
 			}
 		}
 
 		res = x.Quo(y)
 
-		if !res.isSpecial() {
+		if bigres == nil || res.isSpecial() {
+			buf = res.Append(buf[:0], ".3e")
+			idx := bytes.IndexByte(buf, 'e')
+			if idx != -1 {
+				buf[idx-1] = '0'
+			}
+
+			fltx := x.Float64()
+			if res.isInf() && !x.IsZero() && y.IsZero() && fltx == 0 {
+				if x.Signbit() {
+					fltx = -1
+				} else {
+					fltx = 1
+				}
+			}
+
+			fltres := fltx / y.Float64()
+			cmpbuf = fmt.Appendf(cmpbuf[:0], "%.3e", fltres)
+			idx = bytes.IndexByte(cmpbuf, 'e')
+			if idx != -1 {
+				cmpbuf[idx-1] = '0'
+			}
+
+			if string(buf) != string(cmpbuf) {
+				t.Fail()
+			}
+		} else {
 			buf = res.Append(buf[:0], ".3e")
 			idx := bytes.IndexByte(buf, 'e')
 			buf[idx-1] = '0'
 
 			bigres.Quo(bigx, bigy)
-			if bigexp := bigres.MantExp(nil); bigexp < maxUnbiasedExponent && bigexp > minUnbiasedExponent {
-				bigbuf = bigres.Append(bigbuf[:0], 'e', 3)
-				idx = bytes.IndexByte(bigbuf, 'e')
-				bigbuf[idx-1] = '0'
+			if bigexp := bigres.MantExp(nil); bigexp > minBinaryExponent {
+				cmpbuf = bigres.Append(cmpbuf[:0], 'e', 3)
+				idx = bytes.IndexByte(cmpbuf, 'e')
+				cmpbuf[idx-1] = '0'
 
-				if string(buf) != string(bigbuf) {
+				if string(buf) != string(cmpbuf) {
 					t.Fail()
 				}
+			} else if string(buf) != "0.000e+00" {
+				t.Fail()
 			}
 		}
 
 		res = x.Sub(y)
 
-		if !res.isSpecial() {
+		if res.isSpecial() || bigres == nil {
+			buf = res.Append(buf[:0], ".3e")
+			idx := bytes.IndexByte(buf, 'e')
+			if idx != -1 {
+				buf[idx-1] = '0'
+			}
+
+			fltres := x.Float64() - y.Float64()
+			cmpbuf = fmt.Appendf(cmpbuf[:0], "%.3e", fltres)
+			idx = bytes.IndexByte(cmpbuf, 'e')
+			if idx != -1 {
+				cmpbuf[idx-1] = '0'
+			}
+
+			if string(buf) != string(cmpbuf) {
+				t.Fail()
+			}
+		} else {
 			buf = res.Append(buf[:0], ".3e")
 			idx := bytes.IndexByte(buf, 'e')
 			buf[idx-1] = '0'
 
 			bigres.Sub(bigx, bigy)
-			if bigexp := bigres.MantExp(nil); bigexp < maxUnbiasedExponent && bigexp > minUnbiasedExponent {
-				bigbuf = bigres.Append(bigbuf[:0], 'e', 3)
-				idx = bytes.IndexByte(bigbuf, 'e')
-				bigbuf[idx-1] = '0'
+			if bigexp := bigres.MantExp(nil); bigexp > minBinaryExponent {
+				cmpbuf = bigres.Append(cmpbuf[:0], 'e', 3)
+				idx = bytes.IndexByte(cmpbuf, 'e')
+				cmpbuf[idx-1] = '0'
 
-				if string(buf) != string(bigbuf) {
+				if string(buf) != string(cmpbuf) {
 					t.Fail()
 				}
+			} else if string(buf) != "0.000e+00" {
+				t.Fail()
 			}
 		}
 	})
